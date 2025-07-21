98abd6f7235ca1606679d31f32dd493db50a133d
vaibhavnarkhede12 <39726664+vaibhavnarkhede12@users.noreply.github.com>
2025-07-21 17:17:14 +0530

    Update run.bat

--- bq_to_gcs/run.bat
+++ bq_to_gcs/run.bat
@@ -1,17 +1,21 @@
-set GCP_ENV=dev 
-set REGION=europe-west2 
-set NUM_WORKERS=1 
-set MAX_WORKERS=3 
-set PROJECT=myproject-dev 
-set TEMPLATE_BUCKET=gs://%PROJECT%-temp-template 
-set BUCKET=gs://%PROJECT%-esp-tmp 
-set KMS_PROJECT=myproject-63207744-kms-dev 
-set JOB_NAME=bq_to_gcs_bat
+set GCP_ENV=dev
+set REGION=europe-west2
+set MARKET=uk
+set NUM_WORKERS=2
 
-gcloud --project=%PROJECT% dataflow jobs run %JOB_NAME%-%TX% ^ 
---gcs-location=gs://%TEMPLATE_BUCKET%/templates/%JOB_NAME%.json ^ 
---region=%REGION% ^ 
---service-account=event_refinery@%PROJECT%.iam.gserviceaccount.com ^ 
---staging-location=%BUCKET%/ ^ 
---subnetwork=https://www.googleapis.com/compute/v1/projects/%KMS_PROJECT%/regions/%REGION%/subnetworks/cinternal-vpc1-%REGION% ^ 
---num-workers=%NUM_WORKERS%
+set PROJECT=hsbc-113932821-ihubeucl1-%GCP_ENV%
+set TEMPLATE_BUCKET=%PROJECT%-esp-template
+set BUCKET=gs://%PROJECT%-esp-tmp
+set KMS_PROJECT=hsbc-6320774-kms-%GCP_ENV%
+set JOB_NAME=bq_to_gcs_test
+set DT=11062025
+
+gcloud --project=%PROJECT% dataflow jobs run %JOB_NAME%-%DT% ^
+--gcs-location %TEMPLATE_BUCKET%/templates/%JOB_NAME% ^
+--region %REGION% ^
+--service-account-email event-refinery@%PROJECT%.iam.gserviceaccount.com ^
+--staging-location %BUCKET%/tmp ^
+--dataflow-kms-key projects/%KMS_PROJECT%/locations/%REGION%/keyRings/dataFlow/cryptoKeys/dataFlow ^
+--subnetwork https://www.googleapis.com/compute/v1/projects/hsbc-4327957-vpehost-eu-%GCP_ENV%/regions/%REGION%/subnetworks/cinternal-vpc1-%REGION% ^
+--disable-public-ips ^
+--num-workers %NUM_WORKERS%
